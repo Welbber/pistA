ceda09734ce376c43fe0ba77d5757bce01188052
welbber porto <welbervital01@gmail.com>
2022-11-16 11:09:26 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ufcg/ccc/psoft/demo/PistAApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufcg.ccc.psoft.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PistAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PistAApplication.class, args);
+	}
+
+}
