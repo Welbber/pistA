e98c09645ba474177434dcd8b96d04a0326245e8
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2022-11-19 18:40:01 -0300

    criação das classes relacionadas a Pagamento

---
+++ src/main/java/br/com/ufcg/ccc/psoft/dto/PagamentoDTO.java
@@ -0,0 +1,36 @@
+package br.com.ufcg.ccc.psoft.dto;
+
+public class PagamentoDTO {
+
+	private Long id;
+
+	private String tipo;
+
+	private double valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
