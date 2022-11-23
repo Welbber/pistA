9d05a7eb8a746ecaba92eae10106a7a6bbbb5ea8
andrezasantana <andrezasantos0302@gmail.com>
2022-11-23 11:43:06 -0300

    esboço

--- src/main/java/br/com/ufcg/ccc/psoft/model/Cliente.java
+++ src/main/java/br/com/ufcg/ccc/psoft/model/Cliente.java
@@ -1,5 +1,17 @@
 package br.com.ufcg.ccc.psoft.model;
 
+import javax.persistence.Id;
+
 public class Cliente {
+
+    @Id
+    private Long id;
+
+    private String codAcesso;
+
+    private String nomeCompleto;
+
+    private String enderecoPrincipal;
+
 
 }
