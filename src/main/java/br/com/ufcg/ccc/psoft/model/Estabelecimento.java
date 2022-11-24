ca74e750af4e6f490e83dddfe02624053051b0cb
alineebrit <aline.neves@ccc.ufcg.edu.br>
2022-11-23 21:05:21 -0300

    base

--- src/main/java/br/com/ufcg/ccc/psoft/model/Estabelecimento.java
+++ src/main/java/br/com/ufcg/ccc/psoft/model/Estabelecimento.java
@@ -1,10 +1,15 @@
 package br.com.ufcg.ccc.psoft.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Estabelecimento {
 
-	 @Id
+	 	@Id
+	 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
 	 	private String codigoAcesso;
 }
